7a5e30057b36c5d32af265630fd9f5b7f64bb172
zantaiguocheng <kyc1159075055@gmail.com>
2017-02-02 14:48:02 +0800

    no message

---
+++ 小程序/杨辉三角.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+'''Spyder Editor
+
+This is a temporary script file.'''
+
+
+import numpy as np#调用numpy
+k=input()
+x=np.zeros((k,k))#生成由0填充的k*k阶矩阵
+for i in range(k):
+    for j in range(i+1):
+        if j==0:
+            x[i][j]=1
+        else:
+            x[i][j]=x[i-1][j]+x[i-1][j-1]
+        
+print x
